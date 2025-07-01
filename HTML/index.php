d40c6f643620e85db9b4f6e34ce74f4e2e7cb6d8
Bhuwan-Shahi <shahibhuwan265@gmail.com>
2025-07-01 20:04:24 +0545

    php:

---
+++ HTML/index.php
@@ -0,0 +1,36 @@
+<?php
+echo "Hello, World!";
+phpinfo();
+
+
+//Data types : string, number, boolean, undefined, null
+//major data typer : a> Primitive (built in) and b> non primitive (User-defined)
+
+
+//variable display
+// $name = "Bhuwan \n Shahi";
+// echo "$name";
+
+//Direct printing
+echo "Test \n Data";
+
+
+//array 
+$arry1 = ["$name", "email@test.com", 9765225479, 1];
+
+echo $arry1[1];
+echo "<pre>"
+print_r($arry1)
+var_dump($arry1)
+echo "<pre>"
+
+
+//Class and Objects in PHP
+class College{
+    var $name
+    var $address
+    var $phone
+
+    
+}
+
